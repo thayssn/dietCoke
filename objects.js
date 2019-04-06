220ec8a1ea8a722d8bae9a7f766b3719efd717e3
Thays dos Santos <thays.sn@hotmail.com>
2019-04-06 18:34:33 -0300

    recent changes

--- objects.js
+++ objects.js
@@ -5,24 +5,10 @@ let values = [];
 for(item of list) {
   values.push(item.dataset.value)
 }
-console.log(allObjs)
-console.log(list)
-console.log(values)
 
 function ClickableObject (elem) {
   this.id = elem.getAttribute('id');
-  this.secret = '';
-  switch(this.id){
-    case 'OBJ1':
-      this.secret = '1'
-      break;
-    case 'OBJ2':
-      this.secret = '2'
-      break;
-    case 'OBJ3':
-      this.secret = '3'
-      break;
-  }
+  this.secret = elem.dataset.value;
   elem.addEventListener('click', () => {
     if(values.includes(this.secret)){
       let item = list.find( x => x.dataset.value === this.secret);
@@ -33,10 +19,32 @@ function ClickableObject (elem) {
   });
 }
 
+let vw = Math.max(window.innerWidth, window.innerHeight);
+
+let ulY = toVW(window.innerHeight - document.querySelector('ul').getBoundingClientRect().top);
 for(obj of allObjs){
-  let ob = new ClickableObject(obj);
+  let objW =toVW(obj.offsetWidth);
+  let objH = toVW(obj.offsetHeight);
+  let winW = toVW(window.innerWidth);
+  let winH = toVW(window.innerHeight);
+  console.log(winW, winH)
+  let objX = toVW(obj.dataset.x);
+  let objY = toVW(obj.dataset.y);
+  let x = Math.floor(Math.random() * (winW - objW));
+  let y =  Math.floor(Math.random() * (winH - ( objH + ulY)));
+  obj.style.left = (objX || x) + 'vw';
+  obj.style.top = (objY || y) + 'vw';
+  obj.co = new ClickableObject(obj);
 }
 
 window.addEventListener('objectFound', () => {
   console.log('Achou um negócio aqui ó!')
 })
+
+function toVW(n){
+  if(n){
+    return parseInt(((n * 100) / vw).toFixed(2));
+  }else{
+    return false
+  }
+}

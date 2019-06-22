ab94230408cda7d7b189d4491ac573c74b53704e
Thays <thays.sn@hotmail.com>
2019-06-21 23:37:33 -0300

    Melhorias no sistema de posicionamento, estilização, fim de jogo e novas imagens

--- objects.js
+++ objects.js
@@ -1,50 +1,71 @@
-const allObjs = Array.from(document.querySelectorAll('.object'));
-const list = Array.from(document.querySelectorAll('li'));
-let values = [];
+Element.prototype.show = function() {
+  console.log(this);
+  this.style.visibility = "visible";
+  return this;
+};
 
-for(item of list) {
-  values.push(item.dataset.value)
-}
+Element.prototype.hide = function() {
+  console.log(this);
+  this.style.visibility = "hidden";
+};
 
-function ClickableObject (elem) {
-  this.id = elem.getAttribute('id');
+const allObjs = Array.from(document.querySelectorAll(".object"));
+const list = Array.from(document.querySelectorAll("li"));
+const modal = document.querySelector(".congrats");
+
+modal.hide();
+
+let values = list.map(item => {
+  item.addEventListener("objectFound", e => {
+    console.log("Você encontrou:", e.target.textContent);
+  });
+  return item.dataset.value;
+});
+
+function ClickableObject(elem) {
+  this.id = elem.getAttribute("id");
   this.secret = elem.dataset.value;
-  elem.addEventListener('click', () => {
-    if(values.includes(this.secret)){
-      let item = list.find( x => x.dataset.value === this.secret);
-      item.classList.add('found');
-      elem.classList.add('found')
-      window.dispatchEvent(new Event('objectFound'))
+  elem.addEventListener("click", () => {
+    if (values.includes(this.secret)) {
+      let item = list.find(x => x.dataset.value === this.secret);
+      item.classList.add("found");
+      elem.classList.add("found");
+      item.dispatchEvent(new Event("objectFound"));
+      values = values.filter(value => value !== this.secret);
+      if (!values.length) {
+        modal.show();
+      }
     }
   });
 }
 
-let vw = Math.max(window.innerWidth, window.innerHeight);
+let vw = 1440;
 
-let ulY = toVW(window.innerHeight - document.querySelector('ul').getBoundingClientRect().top);
-for(obj of allObjs){
-  let objW =toVW(obj.offsetWidth);
+let ulY = toVW(
+  window.innerHeight - document.querySelector("ul").getBoundingClientRect().top
+);
+
+for (obj of allObjs) {
+  let objW = toVW(obj.offsetWidth);
   let objH = toVW(obj.offsetHeight);
   let winW = toVW(window.innerWidth);
   let winH = toVW(window.innerHeight);
-  console.log(winW, winH)
-  let objX = toVW(obj.dataset.x);
-  let objY = toVW(obj.dataset.y);
-  let x = Math.floor(Math.random() * (winW - objW));
-  let y =  Math.floor(Math.random() * (winH - ( objH + ulY)));
-  obj.style.left = (objX || x) + 'vw';
-  obj.style.top = (objY || y) + 'vw';
+  let x = obj.dataset.x
+    ? toVW(obj.dataset.x)
+    : Math.floor(Math.random() * (winW - objW));
+  let y = obj.dataset.y
+    ? toVW(obj.dataset.y)
+    : Math.floor(Math.random() * (winH - (objH + ulY)));
+  obj.style.left = x + "vw";
+  obj.style.top = y + "vw";
   obj.co = new ClickableObject(obj);
 }
 
-window.addEventListener('objectFound', () => {
-  console.log('Achou um negócio aqui ó!')
-})
-
-function toVW(n){
-  if(n){
-    return parseInt(((n * 100) / vw).toFixed(2));
-  }else{
-    return false
+function toVW(n) {
+  if (n) {
+    let result = (n * 100) / vw;
+    return result.toFixed(3);
+  } else {
+    return false;
   }
 }
